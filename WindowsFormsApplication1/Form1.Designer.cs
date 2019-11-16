034049849bce6a70487792fc91c8bf9e079e7e0e
Juliano Perin <julianoperins@gmail.com>
2019-11-15 19:53:07 -0500

    First Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnShowFlags = new System.Windows.Forms.Button();
+            this.btnHideFlags = new System.Windows.Forms.Button();
+            this.btnChangeFormColortoDefault = new System.Windows.Forms.Button();
+            this.btnChangeLabelColortoDefault = new System.Windows.Forms.Button();
+            this.ChangeFormColortoGreen = new System.Windows.Forms.Button();
+            this.btnChangeColorYellow = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.picGermanyFlag = new System.Windows.Forms.PictureBox();
+            this.picFranceflag = new System.Windows.Forms.PictureBox();
+            this.picfinlandflag = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picGermanyFlag)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFranceflag)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picfinlandflag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Maroon;
+            this.btnStop.Location = new System.Drawing.Point(418, 237);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(88, 35);
+            this.btnStop.TabIndex = 30;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Maroon;
+            this.btnStart.Location = new System.Drawing.Point(52, 237);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(88, 35);
+            this.btnStart.TabIndex = 29;
+            this.btnStart.Text = "Start ";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnShowFlags
+            // 
+            this.btnShowFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowFlags.ForeColor = System.Drawing.Color.Maroon;
+            this.btnShowFlags.Location = new System.Drawing.Point(307, 436);
+            this.btnShowFlags.Name = "btnShowFlags";
+            this.btnShowFlags.Size = new System.Drawing.Size(255, 45);
+            this.btnShowFlags.TabIndex = 28;
+            this.btnShowFlags.Text = "Show Flags";
+            this.btnShowFlags.UseVisualStyleBackColor = true;
+            this.btnShowFlags.Click += new System.EventHandler(this.btnShowFlags_Click);
+            // 
+            // btnHideFlags
+            // 
+            this.btnHideFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHideFlags.ForeColor = System.Drawing.Color.Maroon;
+            this.btnHideFlags.Location = new System.Drawing.Point(19, 436);
+            this.btnHideFlags.Name = "btnHideFlags";
+            this.btnHideFlags.Size = new System.Drawing.Size(255, 45);
+            this.btnHideFlags.TabIndex = 27;
+            this.btnHideFlags.Text = "Hide Flags";
+            this.btnHideFlags.UseVisualStyleBackColor = true;
+            this.btnHideFlags.Click += new System.EventHandler(this.btnHideFlags_Click);
+            // 
+            // btnChangeFormColortoDefault
+            // 
+            this.btnChangeFormColortoDefault.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeFormColortoDefault.ForeColor = System.Drawing.Color.Maroon;
+            this.btnChangeFormColortoDefault.Location = new System.Drawing.Point(307, 368);
+            this.btnChangeFormColortoDefault.Name = "btnChangeFormColortoDefault";
+            this.btnChangeFormColortoDefault.Size = new System.Drawing.Size(255, 45);
+            this.btnChangeFormColortoDefault.TabIndex = 26;
+            this.btnChangeFormColortoDefault.Text = "Change Form Color to Default";
+            this.btnChangeFormColortoDefault.UseVisualStyleBackColor = true;
+            this.btnChangeFormColortoDefault.Click += new System.EventHandler(this.btnChangeFormColortoDefault_Click);
+            // 
+            // btnChangeLabelColortoDefault
+            // 
+            this.btnChangeLabelColortoDefault.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeLabelColortoDefault.ForeColor = System.Drawing.Color.Maroon;
+            this.btnChangeLabelColortoDefault.Location = new System.Drawing.Point(19, 368);
+            this.btnChangeLabelColortoDefault.Name = "btnChangeLabelColortoDefault";
+            this.btnChangeLabelColortoDefault.Size = new System.Drawing.Size(255, 45);
+            this.btnChangeLabelColortoDefault.TabIndex = 25;
+            this.btnChangeLabelColortoDefault.Text = "Change Label Color to Default";
+            this.btnChangeLabelColortoDefault.UseVisualStyleBackColor = true;
+            this.btnChangeLabelColortoDefault.Click += new System.EventHandler(this.btnChangeLabelColortoDefault_Click);
+            // 
+            // ChangeFormColortoGreen
+            // 
+            this.ChangeFormColortoGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeFormColortoGreen.ForeColor = System.Drawing.Color.Maroon;
+            this.ChangeFormColortoGreen.Location = new System.Drawing.Point(307, 300);
+            this.ChangeFormColortoGreen.Name = "ChangeFormColortoGreen";
+            this.ChangeFormColortoGreen.Size = new System.Drawing.Size(255, 45);
+            this.ChangeFormColortoGreen.TabIndex = 24;
+            this.ChangeFormColortoGreen.Text = "Change Form Color to Green";
+            this.ChangeFormColortoGreen.UseVisualStyleBackColor = true;
+            this.ChangeFormColortoGreen.Click += new System.EventHandler(this.ChangeFormColortoGreen_Click);
+            // 
+            // btnChangeColorYellow
+            // 
+            this.btnChangeColorYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeColorYellow.ForeColor = System.Drawing.Color.Maroon;
+            this.btnChangeColorYellow.Location = new System.Drawing.Point(19, 300);
+            this.btnChangeColorYellow.Name = "btnChangeColorYellow";
+            this.btnChangeColorYellow.Size = new System.Drawing.Size(255, 45);
+            this.btnChangeColorYellow.TabIndex = 23;
+            this.btnChangeColorYellow.Text = "Change Color to Yellow";
+            this.btnChangeColorYellow.UseVisualStyleBackColor = true;
+            this.btnChangeColorYellow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Maroon;
+            this.btnExit.Location = new System.Drawing.Point(296, 237);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(88, 35);
+            this.btnExit.TabIndex = 22;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClear.Location = new System.Drawing.Point(174, 237);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 35);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountry.ForeColor = System.Drawing.Color.Red;
+            this.lblCountry.Location = new System.Drawing.Point(87, 167);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(360, 44);
+            this.lblCountry.TabIndex = 20;
+            this.lblCountry.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCountry.Click += new System.EventHandler(this.lblCountry_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(137, 25);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(299, 16);
+            this.lblInstructions.TabIndex = 16;
+            this.lblInstructions.Text = "Click a flag to see the name of the country.";
+            // 
+            // picGermanyFlag
+            // 
+            this.picGermanyFlag.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picGermanyFlag.Image = global::WindowsFormsApplication1.Properties.Resources.Germany;
+            this.picGermanyFlag.Location = new System.Drawing.Point(373, 66);
+            this.picGermanyFlag.Name = "picGermanyFlag";
+            this.picGermanyFlag.Size = new System.Drawing.Size(122, 72);
+            this.picGermanyFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picGermanyFlag.TabIndex = 19;
+            this.picGermanyFlag.TabStop = false;
+            this.picGermanyFlag.Click += new System.EventHandler(this.picGermanyFlag_Click);
+            // 
+            // picFranceflag
+            // 
+            this.picFranceflag.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picFranceflag.Image = global::WindowsFormsApplication1.Properties.Resources.France;
+            this.picFranceflag.Location = new System.Drawing.Point(207, 65);
+            this.picFranceflag.Name = "picFranceflag";
+            this.picFranceflag.Size = new System.Drawing.Size(122, 72);
+            this.picFranceflag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFranceflag.TabIndex = 18;
+            this.picFranceflag.TabStop = false;
+            this.picFranceflag.Click += new System.EventHandler(this.picFranceflag_Click);
+            // 
+            // picfinlandflag
+            // 
+            this.picfinlandflag.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picfinlandflag.Image = global::WindowsFormsApplication1.Properties.Resources.Finland;
+            this.picfinlandflag.Location = new System.Drawing.Point(41, 65);
+            this.picfinlandflag.Name = "picfinlandflag";
+            this.picfinlandflag.Size = new System.Drawing.Size(122, 72);
+            this.picfinlandflag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picfinlandflag.TabIndex = 17;
+            this.picfinlandflag.TabStop = false;
+            this.picfinlandflag.Click += new System.EventHandler(this.picfinlandflag_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 504);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnShowFlags);
+            this.Controls.Add(this.btnHideFlags);
+            this.Controls.Add(this.btnChangeFormColortoDefault);
+            this.Controls.Add(this.btnChangeLabelColortoDefault);
+            this.Controls.Add(this.ChangeFormColortoGreen);
+            this.Controls.Add(this.btnChangeColorYellow);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.picGermanyFlag);
+            this.Controls.Add(this.picFranceflag);
+            this.Controls.Add(this.picfinlandflag);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picGermanyFlag)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFranceflag)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picfinlandflag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnShowFlags;
+        private System.Windows.Forms.Button btnHideFlags;
+        private System.Windows.Forms.Button btnChangeFormColortoDefault;
+        private System.Windows.Forms.Button btnChangeLabelColortoDefault;
+        private System.Windows.Forms.Button ChangeFormColortoGreen;
+        private System.Windows.Forms.Button btnChangeColorYellow;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.PictureBox picGermanyFlag;
+        private System.Windows.Forms.PictureBox picFranceflag;
+        private System.Windows.Forms.PictureBox picfinlandflag;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picfinlandflag_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "Finland";
+        }
+
+        private void picFranceflag_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "France";
+        }
+
+        private void picGermanyFlag_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "Germany";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblCountry.BackColor = Color.Yellow;
+        }
+
+        private void lblCountry_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChangeLabelColortoDefault_Click(object sender, EventArgs e)
+        {
+            lblCountry.BackColor = DefaultBackColor;
+        }
+
+        private void ChangeFormColortoGreen_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void btnChangeFormColortoDefault_Click(object sender, EventArgs e)
+        {
+            this.BackColor = DefaultBackColor;
+        }
+
+        private void btnHideFlags_Click(object sender, EventArgs e)
+        {
+            picfinlandflag.Visible = false;
+            picFranceflag.Visible = false;
+            picGermanyFlag.Visible = false;
+        }
+
+        private void btnShowFlags_Click(object sender, EventArgs e)
+        {
+            picfinlandflag.Visible = true;
+            picFranceflag.Visible = true;
+            picGermanyFlag.Visible = true;
+        }
+    }
+}
